1d9da31728f1680b810bdf599924b10dda111430
Fabian Beuke <mail@beuke.org>
2019-08-17 15:51:22 +0200

    reformat data

--- README.md
+++ README.md
@@ -10,30 +10,41 @@ cnnMarket().then( data => console.log(data) )
 ```
 Example Output:
 ```
-{
-  Dow: '25,864.63',          // Dow Jones Industrial Average
-  DowChgPcnt: '+1.12%',      // Dow Jones Change in Percent
-  DowChg: '+285.24',         // Dow Jones Absolute Change
-  Nasdaq: '7,896.06',
-  NasdaqChgPcnt: '+1.67%',
-  NasdaqChg: '+129.45',
-  SP500: '2,888.21',         // Standard and Poors 500
-  SP500ChgPcnt: '+1.43%',
-  SP500Chg: '+40.61',
-  Japan: '20,418.81',        // NIKKEI 225
-  JapanChgPcnt: '+0.06%',
-  JapanChg: '+13.17',
-  London: '7,117.15',        // FTSE 100
-  LondonChgPcnt: '+0.71%',
-  LondonChg: '+50.50',
-  Germany: '11,562.74',      // DAX 30
-  GermanyChgPcnt: '+1.31%',
-  GermanyChg: '+152.04',
-  Yield10Y: '1.56%',         // U.S. 10 Year Treasury Note (Bond)
-  Yield10YChg: '+0.03',
-  Oil: '$54.62',
-  OilChg: '+0.28',
-  Gold: '$1,523.50',
-  GoldChg: '-0.50'
- }
+[ { symbol: 'DOW',
+    value: '25,886.01',
+    change: '+306.62',
+    changePcnt: '+1.20%' },
+  { symbol: 'NASDAQ',
+    value: '7,895.99',
+    change: '+129.38',
+    changePcnt: '+129.38' },
+  { symbol: 'S&P500',
+    value: '2,888.68',
+    change: '+41.08',
+    changePcnt: '+1.44%' },
+  { symbol: 'Japan',
+    value: '20,418.81',
+    change: '+13.17',
+    changePcnt: '+0.06%' },
+  { symbol: 'HongKong',
+    value: '25,734.22',
+    change: '+241.00',
+    changePcnt: '+0.94%' },
+  { symbol: 'London',
+    value: '7,117.15',
+    change: '+50.50',
+    changePcnt: '+0.71%' },
+  { symbol: 'Germany',
+    value: '11,562.74',
+    change: '+152.04',
+    changePcnt: '+1.31%' },
+  { symbol: 'Yield10y',
+    value: '1.54%',
+    change: '+0.01' },
+  { symbol: 'Oil',
+    value: '$54.94',
+    change: '+0.13' },
+  { symbol: 'Gold',
+    value: '$1,523.60',
+    change: '0.00' } ]
 ```
--- index.js
+++ index.js
@@ -1,53 +1,70 @@
 const scrapeIt = require("scrape-it")
 const { map } = require("lodash/fp")
 
-const url = "http://money.cnn.com/data/markets/"
-const points = i => name => "li:nth-child(" + i + ") span.ticker-" + name
+// Constants
+const url                  = "http://money.cnn.com/data/markets/"
+const col                  = "div.column.double-column > div.column.left-column > "
+const wm                   = "> a > span.world-market-"
+
+// Selector Functions
+const points               = i => name => "li:nth-child(" + i + ") span.ticker-" + name
 const [dow, nasdaq, sp500] = map(points, [1,2,3])
-const nth = i => "div:nth-child(" + i + ") "
-const col = "div.column.double-column > div.column.left-column > "
-const london = i => col + nth(3) + "> div > " + nth(2) + "> " + nth(1)
-    + "> a > span.world-market-" + i
-const germany = i => nth(3) + "> div > " + nth(2) + "> " + nth(2)
-    + "> a > span.world-market-" + i
-const japan = i => nth(1) + nth(1) + nth(1) + "span.world-market-" + i
-const yield10 = i => col + nth(2)
-    + "> ul > li:nth-child(1) > a > span.column.quote-" + i
-const keyStats = (n, i) => col + nth(2) + "> ul > li:nth-child(" + i
-    + ") > a > span.column.quote-" + n
+const nth                  = i => "div:nth-child(" + i + ") "
+const london               = i => col + nth(3) + "> div > " + nth(2) + "> " + nth(1) + wm + i
+const hk                   = i => col + nth(3) + "> div > " + nth(1) + nth(2) + wm + i
+const germany              = i => nth(3) + "> div > " + nth(2) + "> " + nth(2) + wm + i
+const japan                = i => nth(1) + nth(1) + nth(1) + "span.world-market-" + i
+const yield10              = i => col + nth(2) + "> ul > li:nth-child(1) > a > span.column.quote-" + i
+const keyStats             = (n, i) => col + nth(2) + "> ul > li:nth-child(" + i + ") > a > span.column.quote-" + n
+
 
 const selector = {
-    Dow            : dow      ('points'),
-    DowChgPcnt     : dow      ('name-change'),
-    DowChg         : dow      ('points-change'),
-    Nasdaq         : nasdaq   ('points'),
-    NasdaqChgPcnt  : nasdaq   ('name-change'),
-    NasdaqChg      : nasdaq   ('points-change'),
-    SP500          : sp500    ('points'),
-    SP500ChgPcnt   : sp500    ('name-change'),
-    SP500Chg       : sp500    ('points-change'),
-    Japan          : japan    ('change-last'),
-    JapanChgPcnt   : japan    ('change-percent'),
-    JapanChg       : japan    ('change'),
-    London         : london   ('change-last'),
-    LondonChgPcnt  : london   ('change-percent'),
-    LondonChg      : london   ('change'),
-    Germany        : germany  ('change-last'),
-    GermanyChgPcnt : germany  ('change-percent'),
-    GermanyChg     : germany  ('change'),
-    Yield10Y       : keyStats ('col', 1),
-    Yield10YChg    : keyStats ('change', 1),
-    Oil            : keyStats ('col', 2),
-    OilChg         : keyStats ('change', 2),
-    Gold           : keyStats ('col', 5),
-    GoldChg        : keyStats ('change', 5)
+    dow            : dow      ('points'),
+    dowChgPcnt     : dow      ('name-change'),
+    dowChg         : dow      ('points-change'),
+    nasdaq         : nasdaq   ('points'),
+    nasdaqChgPcnt  : nasdaq   ('name-change'),
+    nasdaqChg      : nasdaq   ('points-change'),
+    sp500          : sp500    ('points'),
+    sp500ChgPcnt   : sp500    ('name-change'),
+    sp500Chg       : sp500    ('points-change'),
+    japan          : japan    ('change-last'),
+    japanChgPcnt   : japan    ('change-percent'),
+    japanChg       : japan    ('change'),
+    hk             : hk       ('change-last'),
+    hkChgPcnt      : hk       ('change-percent'),
+    hkChg          : hk       ('change'),
+    london         : london   ('change-last'),
+    londonChgPcnt  : london   ('change-percent'),
+    londonChg      : london   ('change'),
+    germany        : germany  ('change-last'),
+    germanyChgPcnt : germany  ('change-percent'),
+    germanyChg     : germany  ('change'),
+    yield10Y       : keyStats ('col', 1),
+    yield10YChg    : keyStats ('change', 1),
+    oil            : keyStats ('col', 2),
+    oilChg         : keyStats ('change', 2),
+    gold           : keyStats ('col', 5),
+    goldChg        : keyStats ('change', 5)
 }
 
 const cnnMarket = async () => {
-    const { data, _ } = (await (scrapeIt(url, selector)))
-    return data
+    const d = (await (scrapeIt(url, selector))).data
+    return [
+        { symbol: "DOW",      value: d.dow,      change: d.dowChg,     changePcnt: d.dowChgPcnt      },
+        { symbol: "NASDAQ",   value: d.nasdaq,   change: d.nasdaqChg,  changePcnt: d.nasdaqChg       },
+        { symbol: "S&P500",   value: d.sp500,    change: d.sp500Chg,   changePcnt: d.sp500ChgPcnt    },
+        { symbol: "Japan",    value: d.japan,    change: d.japanChg,   changePcnt: d.japanChgPcnt    },
+        { symbol: "HongKong", value: d.hk,       change: d.hkChg,      changePcnt: d.hkChgPcnt       },
+        { symbol: "London",   value: d.london,   change: d.londonChg,  changePcnt: d.londonChgPcnt   },
+        { symbol: "Germany",  value: d.germany,  change: d.germanyChg, changePcnt: d.germanyChgPcnt  },
+        { symbol: "Yield10y", value: d.yield10Y, change: d.yield10YChg                               },
+        { symbol: "Oil",      value: d.oil,      change: d.oilChg                                    },
+        { symbol: "Gold",     value: d.gold,     change: d.goldChg                                   },
+    ]
 }
 
 module.exports = {
     cnnMarket
 }
+
